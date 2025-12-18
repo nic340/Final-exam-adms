9e7374d7bd08f06187834e2c91c6474eb6f9764c
nic340 <nicolediones5@gmail.com>
2025-12-18 06:41:21 -0800

    Update load.py

--- load.py
+++ load.py
@@ -8,21 +8,20 @@ def create_big_table():
     transform_conn = sqlite3.connect(TRANSFORM_DB)
     presentation_conn = sqlite3.connect(PRESENTATION_DB)
     
-    # Get all transformed tables
     tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table'", transform_conn)
-    
     all_dfs = []
+    
     for table in tables['name']:
         df = pd.read_sql(f"SELECT * FROM {table}", transform_conn)
         all_dfs.append(df)
         print(f"Loaded {table} for BIG TABLE")
     
-    # Combine all dataframes
-    big_df = pd.concat(all_dfs, ignore_index=True)
-    
-    # Save to presentation DB
-    big_df.to_sql("big_table", presentation_conn, if_exists='replace', index=False)
-    print("BIG TABLE created in presentation.db")
+    if all_dfs:
+        big_df = pd.concat(all_dfs, ignore_index=True)
+        big_df.to_sql("big_table", presentation_conn, if_exists='replace', index=False)
+        print("BIG TABLE created in presentation.db")
+    else:
+        print("No transformed tables found to combine.")
     
     transform_conn.close()
     presentation_conn.close()
